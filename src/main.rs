a5283f4d9c1a211acd8cf6da91ed7fc886e81e4f
geek-frio <guangzefrio@gmail.com>
2022-01-18 17:25:30 +0800

    xx

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ mod db;
 mod gen;
 use db::init_db;
 use gen::gen_data_binary;
-use std::sync::mpsc;
+use rocksdb::{DBWithThreadMode, SingleThreaded};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use uuid::Uuid;
 
 fn test_point_put_max_ops() {
-    let dbs = init_db();
+    let dbs = Arc::new(Mutex::new(init_db()));
 
+    let dbs_w = dbs.clone();
     let (tx, rx) = mpsc::channel();
     // writer
     let w = thread::spawn(move || {
@@ -30,23 +32,26 @@ fn test_point_put_max_ops() {
             let now = Instant::now();
             let qps = 50000;
             let mut db_idx = 0;
-            //
             println!("current qps is {}", qps);
             for i in 0..qps {
                 let uuid = Uuid::new_v4().to_string();
                 let data: String = gen_data_binary();
 
+                let dbs = dbs_w.lock().unwrap();
                 dbs[db_idx].put(uuid.as_bytes(), data.as_bytes()).unwrap();
                 if i % 10000 == 1 {
-                    println!("key:{}", uuid);
                     tx.send(uuid).unwrap();
                 }
                 total_num += 1;
                 db_idx %= dbs.len();
+                drop(dbs);
             }
             let elapse = now.elapsed().as_millis();
             total_time += elapse;
-            println!("到目前为止导入时速: {}", total_num * 1000 / total_time);
+            println!(
+                "写#############到目前为止导入时速: {}",
+                total_num * 1000 / total_time
+            );
             println!("elpase:{}ms", elapse);
             if elapse < 1000 {
                 sleep(Duration::from_millis((1000 - elapse) as u64))
@@ -59,15 +64,18 @@ fn test_point_put_max_ops() {
     let r = thread::spawn(move || {
         let mut total_time = 0;
         let mut num = 0;
+        // delay 查询, 尽量不命中缓存
+        sleep(Duration::from_secs(60));
         loop {
             let query_key_r = rx.recv();
             if let Ok(query_key) = query_key_r {
-                let (_, time) = key_get(query_key);
-                println!("本次查询耗时:{} micros; ", time);
+                let (_, time) = key_get(query_key, dbs.clone());
+                println!("读@@@@@@@@@@@@本次查询耗时:{} micros; ", time);
                 total_time += time;
                 num += 1;
 
-                println!("平均耗时:{} micro ", total_time / num);
+                println!("读@@@@@@@@@@@@平均耗时:{} micro ", total_time / num);
+                sleep(Duration::from_secs(1));
             } else {
                 return;
             }
@@ -78,12 +86,11 @@ fn test_point_put_max_ops() {
     let _ = r.join();
 }
 
-fn key_get(key: String) -> (String, u128) {
-    let mut dbs = init_db();
-    let db = dbs.pop().unwrap();
+fn key_get(key: String, dbs: Arc<Mutex<Vec<DBWithThreadMode<SingleThreaded>>>>) -> (String, u128) {
+    let dbs = dbs.lock().unwrap();
     let now = Instant::now();
     (
-        String::from_utf8(db.get(key.as_bytes()).unwrap().unwrap()).unwrap(),
+        String::from_utf8(dbs[0].get(key.as_bytes()).unwrap().unwrap()).unwrap(),
         now.elapsed().as_micros(),
     )
 }
