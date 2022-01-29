884ec3d5cd258a78d83821a23b7e0ef178211724
geek-frio <guangzefrio@gmail.com>
2022-01-29 16:08:14 +0800

    basically client/server

--- cmd/skdb-ctl/src/main.rs
+++ cmd/skdb-ctl/src/main.rs
@@ -1,4 +1,13 @@
+use std::sync::Arc;
+
 use clap::Parser;
+use futures::SinkExt;
+use grpcio::ChannelBuilder;
+use grpcio::Environment;
+use grpcio::WriteFlags;
+use skdb::kv::service::tracing::StreamReqData;
+use skdb::kv::service::tracing_grpc::SkyTracingClient;
+use skdb::TOKIO_RUN;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -14,4 +23,19 @@ pub struct Args {
 fn main() {
     let args = Args::parse();
     println!("port:{}, ip:{}", args.port, args.ip);
+    let env = Environment::new(3);
+    let channel = ChannelBuilder::new(Arc::new(env)).connect("127.0.0.1:9000");
+    let client = SkyTracingClient::new(channel);
+    let duplex = client.push_msgs().unwrap();
+    let mut sink = duplex.0;
+    // let source = duplex.1;
+
+    TOKIO_RUN.block_on(async move {
+        println!("Start to send data..");
+        let mut req_data = StreamReqData::new();
+        req_data.data = "xxxxxxxlll".to_string();
+        let tuple = (req_data, WriteFlags::default());
+        let res = sink.send(tuple).await;
+        println!("sink res is:{:?}", res);
+    });
 }
--- src/kv/service/mod.rs
+++ src/kv/service/mod.rs
@@ -25,26 +25,4 @@ impl tracing_grpc::SkyTracing for SkyTracingService {
 }
 
 #[cfg(test)]
-mod tests {
-    use std::sync::Arc;
-
-    use super::tracing_grpc;
-    use crate::kv::service::SkyTracingService;
-    use ::grpcio::ServerBuilder;
-    use grpcio::Environment;
-    #[test]
-    fn test_start_server() {
-        let sky_tracing = SkyTracingService;
-        let service = tracing_grpc::create_sky_tracing(sky_tracing);
-        let env = Environment::new(1);
-        let mut server = ServerBuilder::new(Arc::new(env))
-            .bind("127.0.0.1", 9000)
-            .register_service(service)
-            .build()
-            .unwrap();
-        println!("Server started!");
-        server.start();
-        use std::thread;
-        thread::park();
-    }
-}
+mod tests {}
