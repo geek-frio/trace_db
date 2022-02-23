12f295e91c890470108fc9071935e8ba57226554
geek-frio <guangzefrio@gmail.com>
2022-02-23 16:55:04 +0800

    Add handshake service

---
+++ cmd/skdb-server/src/serv/mod.rs
@@ -0,0 +1,2 @@
+pub mod hand;
+pub mod service;
--- cmd/skdb-server/src/serv/service.rs
+++ cmd/skdb-server/src/serv/service.rs
@@ -1,27 +1,12 @@
-use std::sync::Arc;
-
-use clap::Parser;
 use futures::SinkExt;
 use futures::TryStreamExt;
 use futures_util::{FutureExt as _, TryFutureExt as _, TryStreamExt as _};
 use grpcio::*;
 use skproto::tracing::*;
-
-mod serv;
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-pub struct Args {
-    #[clap(short, long, default_value_t = 9000)]
-    port: u16,
-
-    #[clap(short, long, default_value = "127.0.0.1")]
-    ip: String,
-}
-
-#[derive(Clone)]
 struct SkyTracingService;
+
 impl SkyTracing for SkyTracingService {
+    // Just for push msg test
     fn push_msgs(
         &mut self,
         ctx: ::grpcio::RpcContext,
@@ -43,21 +28,12 @@ impl SkyTracing for SkyTracingService {
 
         ctx.spawn(f)
     }
-}
 
-fn main() {
-    let args = Args::parse();
-    println!("service port:{}, ip:{}", args.port, args.ip);
-
-    let skytracing = SkyTracingService;
-    let env = Environment::new(1);
-    let service = create_sky_tracing(skytracing);
-    let mut server = ServerBuilder::new(Arc::new(env))
-        .bind(args.ip, args.port)
-        .register_service(service)
-        .build()
-        .unwrap();
-    server.start();
-    use std::thread;
-    thread::park();
+    fn push_segments(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        _stream: ::grpcio::RequestStream<SegmentData>,
+        sink: ::grpcio::DuplexSink<SegmentRes>,
+    ) {
+    }
 }
