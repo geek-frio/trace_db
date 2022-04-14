34039953845e1591eba26efa0b3bc882a5e72ba6
geek-frio <guangzefrio@gmail.com>
2022-04-14 15:18:39 +0800

    FIX

--- src/com/ring.rs
+++ src/com/ring.rs
@@ -1,6 +1,9 @@
 use std::iter::Chain;
 use std::slice::Iter;
 
+pub trait SeqId {
+    fn seq_id(&self) -> u64;
+}
 // RingQueue's length should be bigger
 // We only care about data between ack position and send position
 pub struct RingQueue<E> {
@@ -8,12 +11,18 @@ pub struct RingQueue<E> {
     ack_pos: usize,
     send_pos: usize,
     size: usize,
+    start_num: u64,
 }
 
 enum RingIter<'a, T> {
     Chain(Chain<Iter<'a, T>, Iter<'a, T>>),
     Single(Iter<'a, T>),
     Empty,
+}
+
+enum RingQueueError {
+    SendNotOneByOne,
+    QueueIsFull,
 }
 
 impl<'a, T> Iterator for RingIter<'a, T> {
@@ -28,30 +37,34 @@ impl<'a, T> Iterator for RingIter<'a, T> {
     }
 }
 
-impl<E> RingQueue<E> {
-    fn new(size: usize) -> RingQueue<E> {
+impl<E> RingQueue<E>
+where
+    E: SeqId,
+{
+    fn new(size: usize, start_num: u64) -> RingQueue<E> {
         RingQueue {
             data: Vec::new(),
             ack_pos: 0,
             send_pos: 0,
             size,
+            start_num,
         }
     }
 
-    fn insert(&mut self, el: E) -> bool {
+    fn send(&mut self, el: E) -> Result<(), RingQueueError> {
         if self.is_full() {
-            return false;
+            return Err(RingQueueError::QueueIsFull);
+        }
+        if el.seq_id() - self.start_num != 1 {
+            return Err(RingQueueError::SendNotOneByOne);
         }
         self.send_pos = self.send_pos + 1 % self.size;
         self.data.insert(self.send_pos, el);
-        true
+        Ok(())
     }
 
     fn is_full(&self) -> bool {
-        if self.send_pos < self.ack_pos {
-            return self.send_pos + self.ack_pos == self.size;
-        }
-        false
+        self.send_pos < self.ack_pos && self.send_pos + self.ack_pos == self.size
     }
 
     fn not_ack_iter(&self) -> RingIter<'_, E> {
@@ -65,4 +78,10 @@ impl<E> RingQueue<E> {
             RingIter::Single(self.data.as_slice()[self.ack_pos..self.send_pos].iter())
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_send_ack() {}
 }
