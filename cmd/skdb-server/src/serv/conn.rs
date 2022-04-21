ac380d997809a60ff854620130824027cd4ec97d
geek-frio <guangzefrio@gmail.com>
2022-04-21 19:27:37 +0800

    change name

--- cmd/skdb-server/src/serv/conn.rs
+++ cmd/skdb-server/src/serv/conn.rs
@@ -9,7 +9,7 @@ use std::{
 use std::sync::Mutex;
 
 pub struct ConnManager {
-    connId: AtomicI32,
+    conn_id: AtomicI32,
     conns: Arc<Mutex<HashMap<i32, ()>>>,
 }
 
@@ -17,12 +17,12 @@ impl ConnManager {
     pub fn new() -> ConnManager {
         ConnManager {
             conns: Arc::new(Mutex::new(HashMap::new())),
-            connId: AtomicI32::new(1),
+            conn_id: AtomicI32::new(1),
         }
     }
 
     pub fn gen_new_conn_id(&self) -> i32 {
-        let id = self.connId.fetch_add(1, Ordering::Relaxed);
+        let id = self.conn_id.fetch_add(1, Ordering::Relaxed);
         let mut guard = self.conns.lock().unwrap();
         guard.insert(id, ());
         id
