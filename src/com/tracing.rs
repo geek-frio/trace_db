fc7324e9bb9cb0121f95dded1405cd6476852274
geek-frio <guangzefrio@gmail.com>
2022-05-10 17:17:23 +0800

    complete tokio tracing subscriber code

--- src/com/tracing.rs
+++ src/com/tracing.rs
@@ -33,13 +33,15 @@ impl RollingFileMaker {
         name_prefix: String,
         log_path: PathBuf,
         rolling_size: usize,
-    ) -> Result<RollingFileMaker, AnyError> {
-        // let (sender, receiver) = crossbeam_channel::bounded(5000);
+        shut_notify: Sender<ReverseFileEvent>,
+    ) -> Result<(RollingFileMaker, Sender<MsgEvent>), AnyError> {
         let (sender, mut receiver) = channel(5000);
-        let writer = RollingFileWriter { sender };
+        let writer = RollingFileWriter {
+            sender: sender.clone(),
+        };
 
         let mut tracing_log_consumer =
-            TracingLogConsumer::new(name_prefix, log_path, rolling_size).await?;
+            TracingLogConsumer::new(name_prefix, log_path, rolling_size, shut_notify).await?;
         TOKIO_RUN.spawn(async move {
             loop {
                 let msg_event = receiver.recv().await;
@@ -71,18 +73,34 @@ impl RollingFileMaker {
                                 }
                             }
                         }
+                        MsgEvent::Shutdown => {
+                            while let Ok(r) = receiver.try_recv() {
+                                if let MsgEvent::Msg(buf) = r {
+                                    let _ = tracing_log_consumer.write(buf.as_slice()).await;
+                                }
+                            }
+                            let _ = tracing_log_consumer.flush().await;
+                            let _ = tracing_log_consumer.shut_notify.send(ReverseFileEvent::Shutdown).await;
+                        }
                     },
                 }
             }
         });
-        Ok(RollingFileMaker { writer })
+        Ok((RollingFileMaker { writer }, sender))
     }
 }
 
 #[derive(Debug)]
-enum MsgEvent {
+pub enum MsgEvent {
     Msg(Vec<u8>),
     Flush,
+    Shutdown,
+}
+
+#[derive(Debug)]
+pub enum ReverseFileEvent {
+    LogFileCreate(PathBuf),
+    Shutdown,
 }
 
 impl<'a> Write for &'a RollingFileWriter {
@@ -132,6 +150,7 @@ struct TracingLogConsumer {
     rolling_size: usize,
     file_num: usize,
     name_prefix: String,
+    shut_notify: Sender<ReverseFileEvent>,
 }
 
 #[async_trait]
@@ -147,14 +166,19 @@ impl RollingAbility for TracingLogConsumer {
             w.flush().await?;
         }
         self.file_num += 1;
-        self.writer =
-            Self::create_writer(self.name_prefix.as_str(), &mut self.log_dir, self.file_num)
-                .await?;
+        let mut log_dir = self.log_dir.clone();
+        self.writer = Self::create_writer(
+            self.name_prefix.as_str(),
+            &mut log_dir,
+            self.file_num,
+            &self.shut_notify,
+        )
+        .await?;
         Ok(())
     }
 
     fn need_rolling(&self) -> bool {
-        self.flushed_bytes > self.rolling_size
+        self.flushed_bytes >= self.rolling_size
     }
 }
 
@@ -214,10 +238,13 @@ impl AsyncWrite for TracingLogConsumer {
 impl TracingLogConsumer {
     async fn new(
         name_prefix: String,
-        mut log_dir: PathBuf,
+        log_dir: PathBuf,
         rolling_size: usize,
+        shut_notify: Sender<ReverseFileEvent>,
     ) -> Result<TracingLogConsumer, AnyError> {
-        let writer = Self::create_writer(name_prefix.as_str(), &mut log_dir, 1).await?;
+        let mut cur_log_dir = log_dir.clone();
+        let writer =
+            Self::create_writer(name_prefix.as_str(), &mut cur_log_dir, 1, &shut_notify).await?;
         Ok(TracingLogConsumer {
             writer,
             log_dir,
@@ -226,6 +253,7 @@ impl TracingLogConsumer {
             rolling_size: rolling_size,
             file_num: 1,
             name_prefix,
+            shut_notify,
         })
     }
 
@@ -233,15 +261,16 @@ impl TracingLogConsumer {
         name_prefix: &str,
         path_buf: &mut PathBuf,
         file_num: usize,
+        event_sender: &Sender<ReverseFileEvent>,
     ) -> Result<Option<BufWriter<File>>, AnyError> {
         let file_name = Self::gen_new_file_name(name_prefix, file_num);
         path_buf.push(file_name);
-
         let file = OpenOptions::new()
             .write(true)
             .create(true)
             .open(&path_buf)
             .await?;
+        let _ = event_sender.try_send(ReverseFileEvent::LogFileCreate(path_buf.clone()));
         Ok(Some(BufWriter::new(file)))
     }
 
@@ -250,90 +279,151 @@ impl TracingLogConsumer {
         let mut final_name = String::new();
         final_name.push_str(s);
         final_name.push_str("-");
-        let time_str = format!("{}", local.format("%Y%m%d-%H%M%S"));
-        final_name.push_str(time_str.as_str());
-        final_name.push_str("-");
+        let date_str = format!("{}", local.format("%Y%m%d-"));
+        final_name.push_str(date_str.as_str());
         final_name.push_str(file_num.to_string().as_str());
+        final_name.push_str("-");
+        let time_str = format!("{}", local.format("%H%M%S-%f"));
+        final_name.push_str(time_str.as_str());
         final_name.push_str(".log");
         return final_name;
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use std::{cmp::Ordering, fs::Metadata, path::Path, time::Duration};
-
-    use tokio::{fs::DirEntry, time::sleep};
-
+mod tracing_log {
     use super::*;
+    use regex::Regex;
+    use std::time::Duration;
+    use tokio::{sync::mpsc::Receiver, time::sleep};
+
+    const TEST_DIR: &'static str = "/tmp";
+    const TEST_PREIFX: &'static str = "test_app";
+
+    fn drop_last_created_log_files() {
+        let mut r = std::fs::read_dir(TEST_DIR).unwrap();
+        while let Some(item) = r.next() {
+            let entry = item.unwrap();
+            if entry.file_name().to_str().unwrap().contains(TEST_PREIFX) {
+                let r = std::fs::remove_file(entry.path().as_path());
+            }
+        }
+    }
 
     async fn create_test_rolling_file_maker(
         log_dir: &str,
         name_prefix: &str,
         rolling_size: usize,
-    ) -> RollingFileMaker {
+    ) -> (
+        RollingFileMaker,
+        Receiver<ReverseFileEvent>,
+        Sender<MsgEvent>,
+    ) {
         let mut log_dir_buf = PathBuf::new();
+        let (shut_notify, shut_recv) = channel(500);
         log_dir_buf.push(log_dir);
-        let maker = RollingFileMaker::init(name_prefix.to_string(), log_dir_buf, rolling_size)
-            .await
-            .expect("RollingFileMaker init failed!");
-        maker
+        let (maker, shutdown_sender) = RollingFileMaker::init(
+            name_prefix.to_string(),
+            log_dir_buf,
+            rolling_size,
+            shut_notify,
+        )
+        .await
+        .expect("RollingFileMaker init failed!");
+        (maker, shut_recv, shutdown_sender)
     }
 
-    async fn list_sorted_log_file(dir: &str) -> Vec<(DirEntry, Metadata)> {
-        let mut r = tokio::fs::read_dir(dir).await.expect("read dir failed");
-        let mut files = vec![];
-        while let Ok(item) = r.next_entry().await {
-            match item {
-                None => break,
-                Some(item) => {
-                    if item.file_name().to_str().unwrap().contains("test_app") {
-                        let meta = item.metadata().await.unwrap();
-                        files.push((item, meta));
-                    }
-                }
+    async fn collect_and_drop_files(
+        shutdown_sender: Sender<MsgEvent>,
+        log_event_receiver: &mut Receiver<ReverseFileEvent>,
+        created_files: &mut Vec<PathBuf>,
+    ) {
+        let _ = shutdown_sender.send(MsgEvent::Shutdown).await;
+        while let ReverseFileEvent::LogFileCreate(p) = log_event_receiver.recv().await.unwrap() {
+            created_files.push(p);
+        }
+        for file in created_files {
+            let r = tokio::fs::remove_file(file.as_path()).await;
+            if let Err(e) = r {
+                println!("Drop file error, e:{:?}", e);
             }
         }
-        files.sort_by(|a, b| {
-            let m1 = a.1.modified().unwrap();
-            let m2 = b.1.modified().unwrap();
-            m1.partial_cmp(&m2).unwrap_or(Ordering::Equal)
-        });
-        files
     }
 
     #[tokio::test]
     async fn test_rolling_init_one_line_test() -> Result<(), AnyError> {
         let buf = "abc";
-        let maker = create_test_rolling_file_maker("/tmp", "test_app", 100).await;
+        let (maker, mut shut_recv, shutdown_sender) =
+            create_test_rolling_file_maker(TEST_DIR, TEST_PREIFX, 100).await;
         let mut w = maker.make_writer();
         w.write(buf.as_bytes()).unwrap();
         w.flush().unwrap();
         sleep(Duration::from_secs(1)).await;
 
-        let mut files = list_sorted_log_file("/tmp").await;
-        let last = files.pop().unwrap().0.path();
-        let file_name = last.as_path();
-
-        let body = tokio::fs::read_to_string(file_name).await.unwrap();
-        assert!(body == buf);
+        let mut created_files = vec![];
+        let event = shut_recv.recv().await.unwrap();
+        if let ReverseFileEvent::LogFileCreate(p) = event {
+            let body = tokio::fs::read_to_string(p.as_path()).await.unwrap();
+            println!("body is:{}, buf is:{}", body, buf);
+            assert!(body == buf);
+            created_files.push(p);
+        }
+        collect_and_drop_files(shutdown_sender, &mut shut_recv, &mut created_files).await;
         Ok(())
     }
 
-    async fn check_file_body(path: &Path, body: &str) {}
-
     #[tokio::test]
-    async fn test_muliple_lines() -> Result<(), AnyError> {
-        let maker = create_test_rolling_file_maker("/tmp", "test_app", 100).await;
+    async fn test_multiple_lines() -> Result<(), AnyError> {
+        let (maker, mut log_create_event_recv, shutdown_sender) =
+            create_test_rolling_file_maker(TEST_DIR, TEST_PREIFX, 100).await;
         let mut w = maker.make_writer();
+        let matches_num = 10;
         let mut s = String::new();
         let test_str = "1234567890";
         for _ in 0..10 {
             let _ = w.write(test_str.as_bytes());
             s.push_str(test_str);
         }
-        w.flush();
+        let _ = w.flush();
+        sleep(Duration::from_millis(500)).await;
 
+        let mut created_files = vec![];
+        let event = log_create_event_recv.recv().await.unwrap();
+        if let ReverseFileEvent::LogFileCreate(p) = event {
+            let body = tokio::fs::read_to_string(p.as_path()).await.unwrap();
+            created_files.push(p);
+            let re = Regex::new(r"1234567890").unwrap();
+            let matches = re.find_iter(body.as_str());
+            assert!(matches.count() == matches_num);
+        }
+
+        collect_and_drop_files(
+            shutdown_sender,
+            &mut log_create_event_recv,
+            &mut created_files,
+        )
+        .await;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_rolling_ability() -> Result<(), AnyError> {
+        let (maker, mut log_create_recv, shutdown_notify) =
+            create_test_rolling_file_maker(TEST_DIR, TEST_PREIFX, 100).await;
+        let mut w = maker.make_writer();
+        let matches_num = 100;
+        let test_str = "1234567890";
+        for _ in 0..matches_num {
+            let _ = w.write(test_str.as_bytes());
+            let _ = w.flush();
+        }
+        sleep(Duration::from_millis(500)).await;
+        let _ = shutdown_notify.send(MsgEvent::Shutdown).await;
+        let mut created_files = vec![];
+        while let ReverseFileEvent::LogFileCreate(p) = log_create_recv.recv().await.unwrap() {
+            created_files.push(p);
+        }
+        assert!(created_files.len() == 11);
         Ok(())
     }
 }
