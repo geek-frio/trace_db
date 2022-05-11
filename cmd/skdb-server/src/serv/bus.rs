08022cec7012f63a57daed2d1effb076f2d051ae
geek-frio <guangzefrio@gmail.com>
2022-05-11 10:29:47 +0800

    test case for remote message poller

--- cmd/skdb-server/src/serv/bus.rs
+++ cmd/skdb-server/src/serv/bus.rs
@@ -53,26 +53,124 @@ where
             match select(left.next(), right.next()).await {
                 Either::Left((seg_data, _)) => {
                     if let Some(seg) = seg_data {
-                        let r = ProtoLogic::execute(
-                            seg,
-                            &mut self.sink,
-                            &mut self.ack_win,
-                            ack_s.clone(),
-                            &mut self.local_sender,
-                        )
-                        .await;
-                        if let Err(e) = r {
-                            error!("Has met seriously problem, e:{:?}", e);
-                            return;
+                        if !cfg!(test) {
+                            let r = ProtoLogic::execute(
+                                seg,
+                                &mut self.sink,
+                                &mut self.ack_win,
+                                ack_s.clone(),
+                                &mut self.local_sender,
+                            )
+                            .await;
+                            if let Err(e) = r {
+                                error!("Has met seriously problem, e:{:?}", e);
+                                return;
+                            }
+                        } else {
+                            println!("seg:{:?}", seg);
                         }
                     }
                 }
                 // Callback ack seqid;
                 Either::Right((seq_id, _)) => {
-                    let _ = ProtoLogic::handle_callback(&mut self.sink, &mut self.ack_win, seq_id)
-                        .await;
+                    if !cfg!(test) {
+                        let _ =
+                            ProtoLogic::handle_callback(&mut self.sink, &mut self.ack_win, seq_id)
+                                .await;
+                    } else {
+                        println!("Has received acked seq_id");
+                    }
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test_remote_msg_poller {
+    use super::*;
+    use chrono::Local;
+    use skdb::test::gen::*;
+    use skproto::tracing::{Meta, Meta_RequestType};
+    use std::task::Poll;
+
+    struct MockSink;
+
+    impl Sink<(SegmentRes, WriteFlags)> for MockSink {
+        type Error = std::io::Error;
+
+        fn poll_ready(
+            self: Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), Self::Error>> {
+            return std::task::Poll::Ready(Ok(()));
+        }
+
+        fn start_send(
+            self: Pin<&mut Self>,
+            _item: (SegmentRes, WriteFlags),
+        ) -> Result<(), Self::Error> {
+            return Ok(());
+        }
+
+        fn poll_flush(
+            self: Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), Self::Error>> {
+            return std::task::Poll::Ready(Ok(()));
+        }
+
+        fn poll_close(
+            self: Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), Self::Error>> {
+            todo!()
+        }
+    }
+
+    fn mock_seg(conn_id: i32, api_id: i32, seq_id: i64) -> SegmentData {
+        let mut segment = SegmentData::new();
+        let mut meta = Meta::new();
+        meta.connId = conn_id;
+        meta.field_type = Meta_RequestType::TRANS;
+        meta.seqId = seq_id;
+        let now = Local::now();
+        meta.set_send_timestamp(now.timestamp_nanos() as u64);
+        let uuid = uuid::Uuid::new_v4();
+        segment.set_meta(meta);
+        segment.set_trace_id(uuid.to_string());
+        segment.set_api_id(api_id);
+        segment.set_payload(_gen_data_binary());
+        segment.set_zone(_gen_tag(3, 5, 'a'));
+        segment.set_biz_timestamp(now.timestamp_millis() as u64);
+        segment.set_seg_id(uuid.to_string());
+        segment.set_ser_key(_gen_tag(4, 3, 's'));
+        segment
+    }
+
+    struct MockStream {
+        idx: usize,
+        api_id: i32,
+        seq_id: i64,
+    }
+
+    impl Stream for MockStream {
+        type Item = GrpcResult<SegmentData>;
+
+        fn poll_next(
+            self: Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> Poll<Option<Self::Item>> {
+            if self.idx > 100 {
+                return Poll::Ready(None);
+            }
+            let seg = mock_seg(1, self.api_id, self.seq_id);
+            return Poll::Ready(Some(GrpcResult::Ok(seg)));
+        }
+    }
+
+    #[tokio::test]
+    async fn test_loop_poll() -> Result<(), AnyError> {
+        Ok(())
     }
 }
