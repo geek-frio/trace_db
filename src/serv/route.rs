f64476f49b5a9a440e93654d4883718253858d16
geek-frio <guangzefrio@gmail.com>
2022-06-27 14:31:38 +0800

    fix #1: add gracefully shutdown logic to MainServer start

--- src/serv/route.rs
+++ src/serv/route.rs
@@ -1,8 +1,8 @@
 use anyhow::Error as AnyError;
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::{sync::mpsc::UnboundedReceiver, time::sleep};
 use std::{
     collections::HashMap,
-    sync::{atomic::AtomicUsize, Arc, Mutex}, marker::PhantomData, borrow::Cow,
+    sync::{atomic::AtomicUsize, Arc, Mutex}, marker::PhantomData, borrow::Cow, time::Duration,
 };
 use tantivy::{
     schema::{Schema, INDEXED, STORED, STRING, TEXT},
@@ -10,11 +10,13 @@ use tantivy::{
 };
 use tracing::{error, info, trace, trace_span};
 
-use crate::{com::{index::{ConvertIndexAddr, MailKeyAddress}, router::Either, fsm::Fsm}, conf::GlobalConfig};
+use crate::{com::{index::{ConvertIndexAddr, MailKeyAddress}, router::Either, fsm::Fsm}, conf::GlobalConfig, serv::ShutdownEvent, TOKIO_RUN};
 use crate::com::mail::BasicMailbox;
 use crate::tag::engine::*;
 use crate::tag::fsm::TagFsm;
 use crate::{com::router::RouteMsg, tag::{fsm::SegmentDataCallback, engine::TracingTagEngine}};
+
+use super::ShutdownSignal;
 
 pub struct LocalSegmentMsgConsumer<Router, Err> {
     router: Router,
@@ -99,23 +101,56 @@ impl<Router, Err> LocalSegmentMsgConsumer<Router, Err> where Router: RouteMsg<Re
         Ok(())
     }
 
-    pub async fn loop_poll(&mut self) -> Result<(), AnyError> {
+    pub async fn loop_poll(&mut self, shutdown_signal: ShutdownSignal) -> Result<(), AnyError> {
+        let mut recv = shutdown_signal.recv;
+        let drop_notify = shutdown_signal.drop_notify;
         loop {
-            let segment_callback = self.receiver.recv().await;
-
-            match segment_callback {
-                Some(segment_callback) => {
-                    let _consume_span = trace_span!(
-                        "recv_and_process",
-                        trace_id = ?segment_callback.data.trace_id
-                    )
-                    .entered();
-                    trace!(parent: &segment_callback.span, "Has received the segment, try to route to mailbox.");
-                    if let Err(seg) = self.route_msg(segment_callback) {
-                        seg.callback.callback(seg.data.get_meta().get_seqId());
+            tokio::select! {
+                segment_callback = self.receiver.recv() => {
+                    match segment_callback {
+                        Some(segment_callback) => {
+                            let _consume_span = trace_span!(
+                                "recv_and_process",
+                                trace_id = ?segment_callback.data.trace_id
+                            )
+                            .entered();
+                            trace!(parent: &segment_callback.span, "Has received the segment, try to route to mailbox.");
+                            if let Err(seg) = self.route_msg(segment_callback) {
+                                seg.callback.callback(seg.data.get_meta().get_seqId());
+                            }
+                        }
+                        None => return Err(AnyError::msg("LocalSegmentMsgConsumer's sender has closed")),
                     }
                 }
-                None => return Err(AnyError::msg("LocalSegmentMsgConsumer's sender has closed")),
+                event = recv.recv() => {
+                    match event.unwrap() {
+                        ShutdownEvent::GracefulStop => {
+                            while let Ok(segment_callback) = self.receiver.try_recv() {
+                                info!("loop consume all the segments waiting in the channel");
+                                if let Err(seg) = self.route_msg(segment_callback) {
+                                    seg.callback.callback(seg.data.get_meta().get_seqId());
+                                }
+                            }
+                            info!("Wait more 10 seconds of consuming operation..");
+                            TOKIO_RUN.spawn(async {
+                                info!("Start to counting down!");
+                                for i in (1..11).rev() {
+                                    info!("Shutdown seconds remain: {}", i);
+                                    sleep(Duration::from_secs(1)).await;
+                                }
+                            });
+                            sleep(Duration::from_secs(10)).await;
+                            info!("Batch system bridge route task has shutdown..");
+                            drop(drop_notify);
+                            break Ok(());
+                        }
+                        _ => {
+                            info!("Has receive force shutdown event, quit");
+                            break Ok(());
+                        }
+                    }
+                    
+                }
             }
         }
     }
