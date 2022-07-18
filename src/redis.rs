d3b260666224aa4155a3a9f0590c8ac29e152ccf
geek-frio <guangzefrio@gmail.com>
2022-07-18 17:29:26 +0800

    Add test ttlset dump del test case

--- src/redis.rs
+++ src/redis.rs
@@ -6,8 +6,8 @@ use regex::Regex;
 
 type Secs = i64;
 
-const KEY: &'static str = "SK_DB_SERVER_ADDR";
 pub const LEASE_TIME_OUT: i64 = 15;
+const KEY: &'static str = "SK_DB_SERVER_ADDR";
 
 pub struct RedisAddr {
     addr: String,
@@ -63,6 +63,7 @@ pub(crate) struct Record {
 impl MetaInfo {
     fn is_expired(&self, ttl: Secs) -> bool {
         let local = Local::now().timestamp();
+
         local - self.expire_time >= ttl
     }
 }
@@ -121,22 +122,28 @@ impl RedisTTLSet {
                     if i % 2 != 0 {
                         continue;
                     }
+
                     let field = v
                         .get(i)
                         .ok_or(AnyError::msg("RedisTTL: Convert to string key failed"))?;
+
                     let wstr = <WrapStr as TryFrom<&'_ Value>>::try_from(field)?;
                     let meta_info = <MetaInfo as TryFrom<&'_ Value>>::try_from(
                         v.get(i + 1)
                             .ok_or(AnyError::msg("RedisTTL: Convert to meta info failed!"))?,
                     )?;
+
                     // Expired records will be removed automatically
                     if meta_info.is_expired(self.ttl) {
                         let _ = redis::cmd("HDEL")
                             .arg(KEY)
                             .arg(wstr.0.clone())
                             .query::<Value>(conn)?;
+
+                        tracing::trace!("Have found expired ip record, del:{}", wstr.0.clone());
                         continue;
                     }
+
                     let record = Record {
                         meta: meta_info,
                         sub_key: wstr.0,
@@ -160,6 +167,7 @@ impl RedisTTLSet {
         if record.meta.is_expired(self.ttl) {
             return Err(AnyError::msg("Value has already expired!"));
         }
+
         let _ = redis::cmd("HSET")
             .arg(KEY)
             .arg(record.sub_key)
@@ -188,12 +196,119 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::com::gen;
-    use rand::Rng;
+    use std::panic;
+    use std::sync::atomic::AtomicBool;
+    use std::sync::Arc;
 
     use super::*;
+    use crate::{com::gen, log::init_console_logger};
+    use rand::Rng;
+
+    fn gen_virtual_servers(num: usize) -> Vec<String> {
+        let mut start_num = 0;
+        let mut ip_vec = Vec::new();
+
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let mut conn = client.get_connection().unwrap();
+
+        for _ in 0..num {
+            let mut gen_ip = || {
+                start_num += 1;
+                format!("192.168.0.{}", start_num)
+            };
+
+            let ip = gen_ip();
+            ip_vec.push(ip.clone());
+
+            let meta_s = format!("{}:{}", ip, 9999);
+
+            let redis_ttl: RedisTTLSet = Default::default();
+            let _ = redis_ttl.push(&mut conn, meta_s);
+        }
+
+        ip_vec
+    }
+
+    fn gen_expired_virtual_servers(num: usize) -> Vec<String> {
+        let mut start_num = 0;
+        let mut ip_vec = Vec::new();
+
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let mut conn = client.get_connection().unwrap();
+
+        for _ in 0..num {
+            let mut gen_ip = || {
+                start_num += 1;
+                format!("192.168.0.{}", start_num)
+            };
+
+            let ip = gen_ip();
+            ip_vec.push(ip.clone());
+
+            let meta_s = format!("{}:{}", ip, 9999);
+            let mut record: Record = meta_s.try_into().unwrap();
+            record.meta.expire_time = -1;
+
+            let redis_ttl: RedisTTLSet = Default::default();
+            let _ = redis_ttl.push(&mut conn, record);
+        }
+
+        ip_vec
+    }
+
+    fn setup() {
+        init_console_logger();
+    }
+
     #[test]
-    fn test_get_all() -> Result<(), AnyError> {
+    fn test_ttlset_dump_del() {
+        setup();
+
+        let _virs = gen_expired_virtual_servers(3);
+
+        let is_err = Arc::new(AtomicBool::new(false));
+
+        let spawn_num = 3;
+        let mut handles = Vec::new();
+
+        for _ in 0..spawn_num {
+            let c = is_err.clone();
+
+            let handle = std::thread::spawn(move || {
+                let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+
+                let conn = client.get_connection();
+                match conn {
+                    Ok(mut conn) => {
+                        let ttlset: RedisTTLSet = Default::default();
+                        let res = ttlset.query_all(&mut conn);
+
+                        if let Err(_) = res {
+                            c.store(true, std::sync::atomic::Ordering::Relaxed);
+                        }
+                    }
+                    Err(e) => {
+                        tracing::error!("e:{:?}", e);
+                        c.store(true, std::sync::atomic::Ordering::Relaxed);
+                    }
+                }
+            });
+
+            handles.push(handle);
+        }
+
+        for h in handles.into_iter() {
+            let _ = h.join();
+        }
+
+        if is_err.load(std::sync::atomic::Ordering::Relaxed) {
+            panic!();
+        }
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_ttlset_set_get_all() -> Result<(), AnyError> {
         let client = redis::Client::open("redis://127.0.0.1:6379")?;
         let mut con = client.get_connection()?;
         let redis = RedisTTLSet { ttl: 5 };
@@ -201,8 +316,9 @@ mod tests {
         Ok(())
     }
 
-    #[test]
-    fn test_push() -> Result<(), AnyError> {
+    #[tokio::test]
+    #[ignore]
+    async fn test_ttlset_set_push() -> Result<(), AnyError> {
         let client = redis::Client::open("redis://127.0.0.1:6379")?;
         let mut conn = client.get_connection()?;
 
@@ -238,11 +354,5 @@ mod tests {
             },
             sub_key: gen::_gen_tag(3, 5, 'a'),
         }
-    }
-
-    #[test]
-    fn test_into_redis_addr() {
-        let s = "127.0.0.1:6379";
-        let _: RedisAddr = s.try_into().unwrap();
     }
 }
