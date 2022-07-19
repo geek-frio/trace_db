f7d63544ad322acad362e5c46523013b0caccefe
geek-frio <guangzefrio@gmail.com>
2022-07-19 11:29:16 +0800

    refactor redis method into test util mod package

--- src/com/test_util.rs
+++ src/com/test_util.rs
@@ -48,3 +48,90 @@ pub fn gen_valid_mailkeyadd() -> MailKeyAddress {
         .with_index_addr()
         .unwrap()
 }
+
+pub(crate) mod redis {
+    use redis::Value;
+
+    use crate::redis::{Record, RedisTTLSet, KEY};
+
+    pub(crate) fn gen_virtual_servers(num: usize) -> Vec<String> {
+        let mut start_num = 0;
+        let mut ip_vec = Vec::new();
+
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let mut conn = client.get_connection().unwrap();
+
+        for _ in 0..num {
+            let mut gen_ip = || {
+                start_num += 1;
+                format!("192.168.0.{}", start_num)
+            };
+
+            let ip = gen_ip();
+            ip_vec.push(ip.clone());
+
+            let meta_s = format!("{}:{}", ip, 9999);
+
+            let redis_ttl: RedisTTLSet = Default::default();
+            let _ = redis_ttl.push(&mut conn, meta_s);
+        }
+
+        ip_vec
+    }
+
+    pub(crate) fn gen_expired_virtual_servers(num: usize) -> Vec<String> {
+        let mut start_num = 0;
+        let mut ip_vec = Vec::new();
+
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let mut conn = client.get_connection().unwrap();
+
+        for _ in 0..num {
+            let mut gen_ip = || {
+                start_num += 1;
+                format!("192.168.0.{}", start_num)
+            };
+
+            let ip = gen_ip();
+            ip_vec.push(ip.clone());
+
+            let meta_s = format!("{}:{}", ip, 9999);
+            let mut record: Record = meta_s.try_into().unwrap();
+            record.meta.expire_time = -1;
+
+            let redis_ttl: RedisTTLSet = Default::default();
+            let _ = redis_ttl.push(&mut conn, record);
+        }
+
+        ip_vec
+    }
+
+    pub(crate) fn redis_servers_clear() {
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let conn = &mut client.get_connection().unwrap();
+
+        let val = redis::cmd("HGETALL").arg(KEY).query::<Value>(conn).unwrap();
+        match val {
+            Value::Bulk(vals) => {
+                for i in 0..vals.len() {
+                    if i % 2 == 1 {
+                        continue;
+                    }
+
+                    let ip_port = vals.get(i).unwrap();
+
+                    if let Value::Data(byts) = ip_port {
+                        let s = String::from_utf8(byts.to_vec()).unwrap();
+                        redis::cmd("HDEL")
+                            .arg(KEY)
+                            .arg(s)
+                            .query::<Value>(conn)
+                            .unwrap();
+                    }
+                    let _ = vals.get(i + 1).unwrap();
+                }
+            }
+            _ => {}
+        }
+    }
+}
