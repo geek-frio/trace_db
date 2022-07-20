0d2af5e33339b13be282e5f044af3d305cdfb8f8
geek-frio <guangzefrio@gmail.com>
2022-07-20 11:16:58 +0800

    add some expired check functions

--- src/com/index.rs
+++ src/com/index.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
 use chrono::{TimeZone, Utc};
 use std::path::{Path, PathBuf};
+use tokio::fs::DirEntry;
 
 pub type IndexAddr = i64;
 pub const EXPIRED_DAYS: i64 = 15;
@@ -77,6 +78,49 @@ impl MailKeyAddress {
             None => Err(anyhow::Error::msg(
                 "Timestamp converted to u64 type failed, maybe it's a invalid timestamp value",
             )),
+        }
+    }
+
+    pub fn list_expired_dir_items<T: AsRef<Path>>(
+        path: T,
+    ) -> std::io::Result<Vec<std::fs::DirEntry>> {
+        let path = path.as_ref();
+
+        let read_dir = std::fs::read_dir(path)?;
+
+        Ok(read_dir
+            .into_iter()
+            .filter(|item| item.is_ok())
+            .filter(|item| {
+                let entry = item.as_ref().unwrap();
+
+                let file_name = entry.file_name();
+                Self::is_expired_by_filename(file_name.to_str().unwrap(), EXPIRED_DAYS)
+            })
+            .map(|item| item.unwrap())
+            .collect::<Vec<std::fs::DirEntry>>())
+    }
+
+    fn is_expired_by_filename(name: &str, days: i64) -> bool {
+        let re = regex::Regex::new(r"\d{2}\d{2}\d{2}\d").unwrap();
+
+        if re.is_match(name) {
+            let bound_end = Utc::now();
+            let bound_start = bound_end
+                .checked_sub_signed(chrono::Duration::days(days))
+                .unwrap();
+
+            let bound_start_file_name = format!(
+                "{:0>2}{:0>2}{:0>2}{}",
+                bound_start.month(),
+                bound_start.day(),
+                bound_start.hour(),
+                bound_start.minute() / 15
+            );
+
+            name < bound_start_file_name.as_str()
+        } else {
+            false
         }
     }
 }
